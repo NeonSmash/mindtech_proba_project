4a49d2a68746ca60d1cbe5e1396cada82c665e7d
Ferenc Beleczky <35112080+NeonSmash@users.noreply.github.com>
2022-12-23 17:35:26 +0100

    Add files via upload

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -6,60 +6,92 @@ use Illuminate\Http\Request;
 use App\Models\Movies;
 class MoviesController extends Controller
 {
-    
+
     public function index()
     {
         // Get - all data from database
         $movies = Movies::all();
+        $count = 0;
 
-        return response()->json($movies);
+        foreach ($movies as $result) {
+            $count++;
+            echo  $count. ". " . $result->title;
+            echo "<br />";
+        }
     }
+    public function toprated()
+    {
+        // toprated 220
+
+        echo "<a target='_blank' href='https://www.themoviedb.org/'><img src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg' height='50px' alt='The Movie DataBase'/></a>";
+        echo "<br />This product uses the TMDB API but is not endorsed or certified by TMDB.<br />";
+        echo "<br /><u>Top Rated Movies:</u><br /><br />";
+        $count = 0;
+
+        for ($x=1; $x < 12; $x++) {
+    
+            $url_single = "https://api.themoviedb.org/3/movie/top_rated?api_key=93b20005a7082547745bfa44e9379899&page={$x}&language=hu-HU";
+            $json_single = file_get_contents($url_single);
+            $obj_single = json_decode($json_single);
+
+            foreach ($obj_single->results as $result) {
+                $count++;
+                echo  $count. ". " . $result->title;
+                echo "<br />";
+            }
+        }
+        echo "<br />";
+                
+
+                
+            }
+
+    public function show($id)
+    {
+        // give 1 items from movies table
+            $movies = Movies::find($id);
+
+            echo $movies->title;
+        }
+
 
    
     public function store(Request $request)
     {
-        // POST Data to database from user
+        
+         $count = 0;
+        // for ($x=1; $x < 12; $x++) {
+            $x = 1;
+            $url_single = "https://api.themoviedb.org/3/movie/top_rated?api_key=93b20005a7082547745bfa44e9379899&page={$x}&language=hu-HU";
+            $json_single = file_get_contents($url_single);
+            $obj_single = json_decode($json_single);
 
-        // validation
+            $data = $obj_single->results[1];
 
-        $this->validate($request,[
-            'title' => 'required'
-        ]);
+            //$request["title"] = $obj_single->title;
 
-        $movies = new Movies();
-
-        //image data
-
-        // if($request->hasFile('photo')) {
-        //     $file = $request->file('photo');
-        //     $allowedfileExtension = ['pdf','png','jpg'];
-        //     $extension = $file->getClientOriginalExtension();
-        //     $check = in_array($extension, $allowedfileExtension);
-
-        //     if($check) {
-        //         $name = time() . $file->getClientOriginalName();
-        //         $file->move('images', $name);
-        //         $movies->photo = $name;
+        //     foreach ($object_single->results as $result) {
+        //         $count++;
+        //         echo  $count. ". " . $result->title;
+        //         echo "<br />";
         //     }
         // }
 
-        // text data
-        $movies->title = $request->input('title');
+         //validate
+        // $this->validate($request, [
+          //   "title" => "required"
+         //]);
+        
+         // POST Data to database from user
+         $movies = new Movies();
+         $movies->title = $request->input('title');
+         $movies->save();
 
-        $movies->save();
-
-        return response()->json($movies);
+         return response()->json($movies, 201);
     }
 
     
-    public function show($id)
-    {
-        // give 1 items from movies table
-        $movies = Movies::find($id);
-
-        return response()->json($movies);
-    }
-
+    
    
     public function update(Request $request, $id)
     {
@@ -70,12 +102,11 @@ class MoviesController extends Controller
         ]);
 
         $movies = Movies::find($id);
-
         $movies->title = $request->input('title');
-
         $movies->save();
 
         return response()->json($movies);
+        echo "ez frissit";
     }
 
    
@@ -85,5 +116,6 @@ class MoviesController extends Controller
         $movies = Movies::find($id);
         $movies->delete();
         return response()->json('A film sikeresen törölve');
+        echo "ez töröl";
     }
 }
